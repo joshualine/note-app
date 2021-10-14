66a815c5cbcf278c0effd49a702d52e3701ae817
Joshaline <joshuachinwendu@gmail.com>
2021-10-15 00:18:31 +0100

    implemented the search

--- views/index.js
+++ views/index.js
@@ -1,43 +1,73 @@
 // selectors
-// let listNotes = document.getElementById('#listNotes')
-const btn = document.getElementById('btn')
+let filterInput = document.getElementById('searchNotes')
+// let filterValue = document.getElementById('searchNotes').value.toUpperCase();
+
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://127.0.0.1:5000/', true);
+
+xhr.onload = function() {
+  if(this.status == 200) {
+    let listNotes = JSON.parse(this.responseText)
+
+    const checkListNotes = listNotes.data
+    // console.log(checkListNotes)
+    let listNotesOutput = '';
+    for(let oneNote of checkListNotes) {
+      // console.log(oneNote)
+      listNotesOutput += `
+        <div class="listOfNotes" id="listOfNotes">
+          <h4 class="notes-collection" id="notes-collection">
+            ${oneNote}
+            <button class="btn btn-outline-dark" id="btnRead">Read</button>
+            <button class="btn btn-outline-dark" id="btnEdit">Edit</button>
+            <button class="btn btn-outline-dark" id="btnDelete">Delete</button>
+          </h4>
+        </div>
+      `
+      
+    }
+    document.getElementById('listNotes').innerHTML = listNotesOutput;
+    // console.log(listNotesOutput)
+    filterInput.addEventListener('keyup', filterNotes);
+
+    
+    function filterNotes() {
+      let filterValue = document.getElementById('searchNotes').value.toUpperCase();
+      // console.log(filterValue)
+      let div = document.getElementsByClassName('listOfNotes');
+      // console.log(div)
+      for(let i = 0; i<div.length; i++) {
+        let h4 = div[i].getElementsByTagName('h4')[0]
+        if(h4.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+          div[i].style.display = '';
+        } else {
+          div[i].style.display = 'none';
+        }
+      }
+
+
+    }
+  }
+}
+
+xhr.send();
+
+
 
 
 
 // Event listeners
-btn.addEventListener('click', loadNotes);
 
-// let url = 'http://127.0.0.1:5000/';  https://api.github.com/users
+
+
+
 
 
 
 function loadNotes(){
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://127.0.0.1:5000/', true);
-  // xhr.open('GET', 'https://api.github.com/users', true);
-
-  xhr.onload = function() {
-    if(this.status == 200) {
-      let listNotes = JSON.parse(this.responseText)
-
-      // console.log(listNotes)
-      // let listNotesOutput = '';
-      for(let oneNote in listNotes) {
-        console.log(listNotes[oneNote])
-      //   listNotesOutput += `
-      //     <div class="listNote">
-      //       <ul>
-      //         <li>${listNotes[oneNote]}</li>
-      //       </ul>
-      //     </div>
-      //   `
-      }
-      // document.getElementById('listNotes').innerHTML = listNotesOutput;
-    }
-  }
-
-  xhr.send();
-
+  
 //   let xhr = new XMLHttpRequest();
 //   xhr.onreadystatechange = function() {
 //       if (xhr.readyState === 4 && this.status == 200){
